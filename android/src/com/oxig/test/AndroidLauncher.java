461d534c092b7e4d451c4afa531983dff073736d
Poss1Bo1 <mcf@live.nl>
2018-03-18 10:24:18 +0100

    Initial commit

---
+++ android/src/com/oxig/test/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.oxig.test;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.oxig.test.MainClass;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainClass(), config);
+	}
+}
---
+++ desktop/src/com/oxig/test/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.oxig.test.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.oxig.test.MainClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainClass(), config);
+	}
+}
